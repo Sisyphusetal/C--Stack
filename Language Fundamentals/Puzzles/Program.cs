d532c6c69f83976658c4b3b9b5d184a16ca08384
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-02 13:02:02 -0700

    puzzle incomplete

--- Language Fundamentals/Puzzles/Program.cs
+++ Language Fundamentals/Puzzles/Program.cs
@@ -1,12 +1,19 @@
 ﻿//Coin Flip
+static string CoinFlip()
+{
+    Random rand = new Random();
+    int generate = rand.Next(0,2);
 
+    if(generate == 1) {
+        return "Heads";
+    }
+    if(generate == 0) {
+        return "Tails";
+    }
 
-
-
-
-
-
-
+    return "The coin hangs suspended in the air...";
+}
+Console.WriteLine(CoinFlip());
 
 
 //Dice Roll
