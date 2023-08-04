ebdfe8524b4f535025995b52d3dec24198583bd0
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-04 09:01:46 -0700

    Beginning of Game Developer1

--- Language Fundamentals/GameDeveloperOne/Program.cs
+++ Language Fundamentals/GameDeveloperOne/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Attack
+{
+    string Fireball;
+    int DamageAmount
+}
+
+
+class Enemy
+{
+    string Name;
+    int Health;
+    <List> AttackList
+}
