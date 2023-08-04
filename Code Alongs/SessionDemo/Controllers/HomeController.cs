f23cf32964ca212edd7da3ad4ea35e8e529ccc66
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-04 09:27:17 -0700

    Session intro code along

---
+++ Code Alongs/SessionDemo/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SessionDemo.Models;
+
+namespace SessionDemo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        //HttpContext.Session.SetString("UserName", "Winter");
+
+
+        return View();
+    }
+
+    [HttpPost("Login")]
+    public IActionResult Login(string UserName)
+    {
+        HttpContext.Session.SetString("UserName", UserName);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("Player")]
+    public IActionResult Player()
+    {
+        return View();
+    }
+
+    [HttpPost("UpdatePlayer")]
+    public IActionResult UpdatePlayer(string CharacterName, int CharacterLevel)
+    {
+        HttpContext.Session.SetString("CharacterName", CharacterName);
+        HttpContext.Session.SetInt32("CharacterLevel", CharacterLevel);
+
+        return RedirectToAction("Player");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
