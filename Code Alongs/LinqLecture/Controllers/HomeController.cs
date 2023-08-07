73a4bfe2f601d230f0a2f9114e1ecce25912f7c4
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-07 09:16:32 -0700

    Linq Lecture start

---
+++ Code Alongs/LinqLecture/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LinqLecture.Models;
+
+namespace LinqLecture.Controllers;
+
+public class HomeController : Controller
+{
+    public static Game[] AllGames = new Game()
+    {
+        
+    }
+
+
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
