bf3ca0bb31d7665fd396d908a0296cd13cbf23be
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-08 15:44:06 -0700

    Dojo Survery with Model

---
+++ ASP.NET Core/DojoSurveyWithModel/Controllers/SurveyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+using System.Collections.Generic;
+
+namespace DojoSurveyWithModel.Controllers;
+public class SurveyController : Controller  
+{
+    public static List<Survey> Surveys = new List<Survey>();
+
+    [HttpGet("/")]
+    public ViewResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("/info")]
+    public ViewResult Info()
+    {
+        return View("Info", Surveys);
+    }
+
+    [HttpPost("/process")]
+    public IActionResult Process(Survey survey)
+    {
+        if(ModelState.IsValid)
+        {
+            Surveys.Add(survey);
+            return RedirectToAction("Info");
+        }
+        else{
+            return View("Index");
+        }
+    }
+}
+
---
+++ ASP.NET Core/DojoSurveyWithModel/Models/Survey.cs
@@ -0,0 +1,12 @@
+namespace DojoSurveyWithModel.Models;
+
+public class Survey
+{
+    public string Name {get; set;}
+
+    public string Location {get; set;}
+
+    public string FavoriteLanguage {get; set;}
+
+    public string Comments {get; set;}
+}
