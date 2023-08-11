477e8880ff643d314e9e025fce4c919bd3fbb91b
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-11 09:01:14 -0700

    algorithms

--- Month 4 Algorithms/BST.js
+++ Month 4 Algorithms/BST.js
@@ -45,7 +45,7 @@ class BinarySearchTree {
    * @returns {boolean} Indicates if this tree is empty.
    */
   isEmpty() {
-    return this.root === null;
+    return null;
   }
 
   /**
@@ -346,10 +346,98 @@ class BinarySearchTree {
     }
 
     return vals;
-
   }
 
+    /**
+   * BFS order: horizontal rows top-down left-to-right.
+   * Converts this BST into an array following Breadth First Search order.
+   * Example on the fullTree var:
+   * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+   * @param {Node} current The current node during the traversal of this tree.
+   * @returns {Array<number>} The data of all nodes in BFS order.
+   */
+    toArrLevelorder(current = this.root) {
+        let vals = []
+        let newVals = [];
+        if(current === null) {
+          return vals;
+        }
 
+        if(current) {
+          newVals.push(current.data);
+        }
+
+        while(newVals.length > 0) {
+          let whileVals = newVals.shift(); //Shift removes first value from an array and returns it
+          vals.push(whileVals.data);
+
+          if(whileVals.left) {
+            newVals.push(whileVals.left)
+          }
+
+          if(whileVals.right) {
+            newVals.push(whileVals.right);
+          }
+        }
+
+        return vals;
+
+    }
+
+    /**
+     * Recursively counts the total number of nodes in this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during the traversal of this tree.
+     * @returns {number} The total number of nodes.
+     */
+    size(node = this.root) { 
+        let nodes = this.toArrPreorder();
+
+        return nodes.length;
+    }
+  
+    /**
+     * Calculates the height of the tree which is based on how many nodes from
+     * top to bottom (whichever side is taller).
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {number} The height of the tree.
+     */
+    height(node = this.root) { 
+        if(node === null) {
+          return 0;
+        }
+
+        return 1 + Math.max(this.height(node.left), this.height(node.right))
+    }
+  
+    /**
+     * Determines if this tree is a full tree. A full tree is a tree where every
+     * node has both a left and a right except for the leaf nodes (last nodes)
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} node The current node during traversal of this tree.
+     * @returns {boolean} Indicates if this tree is full.
+     */
+    isFull(node = this.root) { 
+      if(!node) {
+        return false;
+      }
+
+      if(!node.left && !node.right)
+      {
+        return true;
+      }
+
+      if(node.left && node.right) {
+        return this.isFull(node.left) && this.isFull(node.right);
+      }
+
+      return false;
+
+    }
 
 
 
