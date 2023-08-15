ecf0f2825127c61883e3725a7e09a31e4b8c2883
Kurt James Clausen <kurtjclausen@gmail.com>
2023-08-15 08:03:59 -0700

    old edits for CRUDelicious

--- ORMs/CRUDelicious/Controllers/HomeController.cs
+++ ORMs/CRUDelicious/Controllers/HomeController.cs
@@ -8,15 +8,44 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private MyContext db;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
     {
         _logger = logger;
+        db = context;
     }
 
     public IActionResult Index()
     {
+        List<Dish> AllDishes = db.Dishes.ToList();
         return View();
     }
+
+    [HttpPost("/dishes/create")]
+    public IActionResult CreateDish(Dish newDish)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Add(newDish);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            //Error handling
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpGet("/dishes/{id}")]
+    public IActionResult ShowDish(int id)
+    {
+        Dish OneDish = db.Dishes.FirstOrDefault(a => a.DishId == id);
+        return View(OneDish);
+    }
+
 
     public IActionResult Privacy()
     {
